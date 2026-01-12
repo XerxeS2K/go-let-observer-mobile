8d712b0c210928a65121e8dee83b946b6cd8a334
XerxeS2K <74686441+XerxeS2K@users.noreply.github.com>
2026-01-12 22:45:50 +0000

    Remove log statement from main_android.go

--- android/main_android.go
+++ android/main_android.go
@@ -3,14 +3,9 @@
 
 package main
 
-import (
-	"log"
-
-	"github.com/ebitengine/gomobile/app"
-)
+import "github.com/ebitengine/gomobile/app"
 
 func main() {
-	log.Println("go-let-observer Android starting...")
 	app.Main(func(a app.App) {
 		NewAndroidApp().Run(a)
 	})
