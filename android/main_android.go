3ee9ff22bce70685ab6cd8c8479442d2d42d4777
XerxeS2K <74686441+XerxeS2K@users.noreply.github.com>
2026-01-12 23:47:30 +0000

    Enhance Android app initialization with logging
    Added logging for application start and set game instance.

--- android/main_android.go
+++ android/main_android.go
@@ -3,10 +3,18 @@
 
 package main
 
-import "github.com/ebitengine/gomobile/app"
+import (
+	"log"
+
+	"github.com/ebitengine/gomobile/app"
+	"github.com/ebitengine/gomobile/mobile"
+)
 
 func main() {
+	log.Println("go-let-observer Android starting")
+
 	app.Main(func(a app.App) {
-		NewAndroidApp().Run(a)
+		game := NewAndroidApp()
+		mobile.SetGame(game)
 	})
 }
