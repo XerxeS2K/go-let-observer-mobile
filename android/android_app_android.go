c21a546e362bff7f515f0acb07c08771ff56582e
XerxeS2K <74686441+XerxeS2K@users.noreply.github.com>
2026-01-12 23:49:57 +0000

    Initialize AndroidApp with logging and dimensions
    Added logging during Android app initialization and set initial width and height.

--- android/android_app_android.go
+++ android/android_app_android.go
@@ -3,22 +3,51 @@
 
 package main
 
-import "github.com/hajimehoshi/ebiten/v2"
+import (
+	"log"
 
-type AndroidApp struct{}
+	"github.com/hajimehoshi/ebiten/v2"
+)
 
-func NewAndroidApp() *AndroidApp {
-	return &AndroidApp{}
+// AndroidApp is the root Ebiten game for Android.
+// It MUST implement ebiten.Game.
+type AndroidApp struct {
+	width  int
+	height int
 }
 
-// Ebiten Game interface
+// NewAndroidApp creates the Android game instance.
+// This is what mobile.SetGame(...) receives.
+func NewAndroidApp() *AndroidApp {
+	log.Println("NewAndroidApp initialised")
+	return &AndroidApp{
+		width:  1280,
+		height: 720,
+	}
+}
+
+// Update is called every tick by Ebiten.
 func (a *AndroidApp) Update() error {
+	// TODO:
+	// - Touch handling
+	// - RCON UI state updates
+	// - Networking / polling
 	return nil
 }
 
+// Draw renders the frame.
 func (a *AndroidApp) Draw(screen *ebiten.Image) {
+	// TODO:
+	// - Draw map
+	// - Draw UI controls
+	// - Draw overlays / debug text
 }
 
-func (a *AndroidApp) Layout(w, h int) (int, int) {
-	return w, h
+// Layout tells Ebiten the logical resolution.
+func (a *AndroidApp) Layout(outsideWidth, outsideHeight int) (int, int) {
+	if outsideWidth > 0 && outsideHeight > 0 {
+		a.width = outsideWidth
+		a.height = outsideHeight
+	}
+	return a.width, a.height
 }
