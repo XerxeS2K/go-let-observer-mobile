d3e5621728ad2647130da2c6056e61d1b8b7e0ce
XerxeS2K <74686441+XerxeS2K@users.noreply.github.com>
2026-01-12 23:39:32 +0000

    Refactor Android app with new methods and properties
    Updated Android app structure to include width and height properties, and added methods for Update, Draw, and Layout.

--- android/android_app_android.go
+++ android/android_app_android.go
@@ -1,21 +1,33 @@
-//go:build android
-// +build android
+//go:build android || mobile
+// +build android mobile
 
 package main
 
 import (
-	"log"
+	"image/color"
 
-	"github.com/ebitengine/gomobile/app"
+	"github.com/hajimehoshi/ebiten/v2"
 )
 
-type AndroidApp struct{}
+type AndroidApp struct {
+	width  int
+	height int
+}
 
 func NewAndroidApp() *AndroidApp {
 	return &AndroidApp{}
 }
 
-func (a *AndroidApp) Run(app.App) {
-	log.Println("Android app running (touch-only stub)")
-	select {} // keep app alive
+func (a *AndroidApp) Update() error {
+	return nil
+}
+
+func (a *AndroidApp) Draw(screen *ebiten.Image) {
+	screen.Fill(color.RGBA{20, 20, 20, 255})
+}
+
+func (a *AndroidApp) Layout(outsideWidth, outsideHeight int) (int, int) {
+	a.width = outsideWidth
+	a.height = outsideHeight
+	return outsideWidth, outsideHeight
 }
