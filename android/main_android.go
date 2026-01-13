943bf91277af3cb4d8c4b043269b8a1cd1bf941a
XerxeS2K <74686441+XerxeS2K@users.noreply.github.com>
2026-01-13 00:32:20 +0000

    Update main_android.go

--- android/main_android.go
+++ android/main_android.go
@@ -1,22 +1,18 @@
-//go:build android || mobile
-// +build android mobile
+//go:build android
+// +build android
 
 package main
 
 import (
 	"log"
 
-	"github.com/ebitengine/gomobile/app"
+	gomobileapp "github.com/ebitengine/gomobile/app"
 	"github.com/hajimehoshi/ebiten/v2/mobile"
 )
 
 func main() {
 	log.Println("go-let-observer Android starting...")
-
-	app.Main(func(a app.App) {
-		// IMPORTANT:
-		// - Use github.com/ebitengine/gomobile/app (NOT golang.org/x/mobile/app)
-		// - Use github.com/hajimehoshi/ebiten/v2/mobile for SetGame
+	gomobileapp.Main(func(a gomobileapp.App) {
 		mobile.SetGame(NewAndroidApp())
 	})
 }
